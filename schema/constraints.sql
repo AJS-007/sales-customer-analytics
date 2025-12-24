074a0a2f5066db3cd810451541d8905e66a8fd26
AJS-007 <janjali9430101834@gmail.com>
2025-12-24 21:58:38 +0530

    Update constraints.sql

--- schema/constraints.sql
+++ schema/constraints.sql
@@ -1,11 +1,1 @@
-ALTER TABLE Sales
-ADD CONSTRAINT fk_region
-FOREIGN KEY (region_id) REFERENCES Regions(region_id);
 
-ALTER TABLE Sales
-ADD CONSTRAINT fk_product
-FOREIGN KEY (product_id) REFERENCES Products(product_id);
-
-ALTER TABLE Sales
-ADD CONSTRAINT fk_customer
-FOREIGN KEY (customer_id) REFERENCES Customers(customer_id);
